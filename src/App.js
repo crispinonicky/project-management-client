528b50aea9e6ccf538311c2b30b9f621e3902cee
Nicholas Crispino <crispinonicky@gmail.com>
2018-09-28 10:47:39 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AddProject from './components/AddProject';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+{/* <AddProject/> */}
+
+      </div>
+    );
+  }
+}
+
+export default App;
